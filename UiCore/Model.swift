1f2cb2e2a0d86831f1484d46c42f3e203ac14bdc
Marsxan <mr.mars1992@gmail.com>
2017-11-25 09:47:31 +0330

    let's Go

---
+++ UiCore/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  UiCore
+//
+//  Created by Apple on 9/4/1396 AP.
+//  Copyright © 1396 Parsa. All rights reserved.
+//
+
+import Foundation
+public class Model{
+    public var name: [String]
+    
+    public init() {
+        name=["a","b"]
+    }
+}
